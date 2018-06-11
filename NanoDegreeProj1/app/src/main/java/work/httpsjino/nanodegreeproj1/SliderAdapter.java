bdd12dbd1035bed03de2c0cdff8f0db55b2be7a8
Jino Jossy <jinojossy93@gmail.com>
2018-06-11 11:26:35 +0530

    initial commit with README

---
+++ NanoDegreeProj1/app/src/main/java/work/httpsjino/nanodegreeproj1/SliderAdapter.java
@@ -0,0 +1,54 @@
+package work.httpsjino.nanodegreeproj1;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+public class SliderAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private List<Integer> color;
+    private List<String> colorName;
+//    private Integer[] images = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3};
+    private Integer[] images = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4};
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.activity_main, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ViewPager viewPager = (ViewPager) container;
+        View view = (View) object;
+        viewPager.removeView(view);
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Udacity-Google-India-Challenge-Scholarship-Phase-2-Project-1
+This is my first project for Udacity Google India Challenge Scholarship in Android Basics Course 
+
+![Badge](https://github.com/jinojossy93/Challenge-Scholarship-Phase-2-Project-1/blob/master/Badge.png)
+
+---
+
+| # | Title | Description |
+| --- | --- | --- |
+| 1 | [Jino Jossy](https://github.com/jinojossy93/Google-India-Challenge-Scholarship-Phase-2-Project-1/tree/master/JinoJossy) | Single Screen App |
+
+---
+
+# [Project 1 - PiedPiper - Build a Single Screen App](https://github.com/jinojossy93/Google-India-Challenge-Scholarship-Phase-2-Project-1/tree/master/JinoJossy)
+
+Design and implement a single screen app that displays information about me :).
+
+### About
+
+Implemented a single screen app for Google-India-Challenge-Scholarship
