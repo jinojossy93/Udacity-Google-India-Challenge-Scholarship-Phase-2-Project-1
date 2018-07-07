a8eeb665d2b56fca1d48ab5a0994ce6ef4b71f09
Jino Jossy <jinojossy93@gmail.com>
2018-07-07 19:59:53 +0530

    added new court counter app and restructured the GitHub top to bottom

---
+++ Projects/NanoDegreeProj1/app/src/main/java/work/httpsjino/nanodegreeproj1/MainActivity.java
@@ -0,0 +1,96 @@
+package work.httpsjino.nanodegreeproj1;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        viewPager = (ViewPager) findViewById(R.id.sliderView);
+
+        SliderAdapter viewPagerAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(), 4000,6000);
+        //Get a reference to your WebView//
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(false);
+        webView.getSettings().setSupportMultipleWindows(false);
+        webView.getSettings().setSupportZoom(false);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.loadUrl("https://jino.work/#contact");
+    }
+
+    /**
+     * This method is called when website or map-address of business is clicked
+     * @param view This is the view which was clicked by user
+     */
+    public void contactUs(View view) {
+        switch (view.getId()) {
+            case R.id.business_website:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://jino.work")));
+                break;
+
+            case R.id.business_mob:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("tel: +91-9746785785")));
+                break;
+
+            case R.id.business_address:
+                Intent openBusinessOnMaps = new Intent(Intent.ACTION_VIEW);
+                openBusinessOnMaps.setData(Uri.parse("geo:8.8831872,76.6731672"));
+
+                if (openBusinessOnMaps.resolveActivity(getPackageManager()) != null)
+                    startActivity(openBusinessOnMaps);
+                break;
+        }
+    }
+
+    public class MyTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (viewPager.getCurrentItem() == 0) {
+                        viewPager.setCurrentItem(1);
+                    }
+                    else if (viewPager.getCurrentItem() == 1) {
+                        viewPager.setCurrentItem(2);
+                    }
+                    else if (viewPager.getCurrentItem() == 2) {
+                        viewPager.setCurrentItem(3);
+                    }
+                    else if (viewPager.getCurrentItem() == 3) {
+                        viewPager.setCurrentItem(4);
+                    }
+                    else if (viewPager.getCurrentItem() == 4) {
+                        viewPager.setCurrentItem(0);
+                    }
+                }
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,20 +1,25 @@
-# Udacity-Google-India-Challenge-Scholarship-Phase-2-Project-1
+# Udacity-Google-India-Challenge-Scholarship-Phase-2-Projects
 This is my first project for Udacity Google India Challenge Scholarship in Android Basics Course 
 
-![Badge](https://github.com/jinojossy93/Udacity-Google-India-Challenge-Scholarship-Phase-2-Project-1/blob/master/Badge.png)
+![Badge](https://github.com/jinojossy93/Udacity-Google-India-Challenge-Scholarship-Phase-2-Projects/blob/master/Badge.png)
 
 ---
 
 | # | Title | Description |
 | --- | --- | --- |
-| 1 | [Jino Jossy](https://github.com/jinojossy93/Udacity-Google-India-Challenge-Scholarship-Phase-2-Project-1/tree/master/NanoDegreeProj1) | Single Screen App |
+| 1 | [Jino Jossy](https://github.com/jinojossy93/Udacity-Google-India-Challenge-Scholarship-Phase-2-Projects/tree/master/Projects/NanoDegreeProj1) | Single Screen App |
+| 2 | [Court Counter](https://github.com/jinojossy93/Udacity-Google-India-Challenge-Scholarship-Phase-2-Projects/tree/master/Projects/NanoDegreeProj2) | Court Counter |
 
 ---
 
-# [Project 1 - PiedPiper - Build a Single Screen App](https://github.com/jinojossy93/Udacity-Google-India-Challenge-Scholarship-Phase-2-Project-1/tree/master/NanoDegreeProj1)
+# [Project 1 - Jino Jossy - Build a Single Screen App](https://github.com/jinojossy93/Udacity-Google-India-Challenge-Scholarship-Phase-2-Projects/tree/master/Projects/NanoDegreeProj1)
 
 Design and implement a single screen app that displays information about me :)
 
+# [Project 2 - Court Counter - Build a Single Screen App for keeping court count](https://github.com/jinojossy93/Udacity-Google-India-Challenge-Scholarship-Phase-2-Projects/tree/master/Projects/NanoDegreeProj2)
+
+Design and implement a single screen app for keeping court count of a football match.
+
 ### About
 
-Implemented a single screen app for Google-India-Challenge-Scholarship
+Common hosting for apps completed as part of Google-India-Challenge-Scholarship
